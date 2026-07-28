b225f6072944de6d2f14d2c8c8830befcb6b3749
Hemant-Bajaj <hemant825bajaj@gmail.com>
2026-07-28 23:15:35 +0530

    project completed

--- utils/currency_converter.py
+++ utils/currency_converter.py
@@ -13,4 +13,6 @@ class CurrencyConverter:
         rates = response.json()["conversion_rates"]
         if to_currency not in rates:
             raise ValueError(f"{to_currency} not found in exchange rates.")
+        print(amount, type(amount))
+        print(rates[to_currency], type(rates[to_currency]))
         return amount * rates[to_currency]
